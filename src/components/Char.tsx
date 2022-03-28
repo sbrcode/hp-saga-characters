9fa54173bfa54a765c8b34054813ce372117d801
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2022-03-28 11:06:38 +0200

    refresh-char-ok

--- src/components/Char.tsx
+++ src/components/Char.tsx
@@ -6,32 +6,33 @@ import { CharContext } from "../contexts/CharContext"
 const Char: React.FC = () => {
   let params = useParams();
   const { fans } = useContext(CharContext as never);
-
-  console.log('CHAR', params, fans.length)
-
   const getChar = (char: string | undefined) => _.find(fans, { 'name': char })
 
-  const {
-    name, alternate_names, species, gender, house, dateOfBirth, wizard, ancestry, hairColour,
-    hogwartsStudent, hogwartsStaff, alive, // wand, image
-        } = getChar(params.id);
+  if (fans && fans.length > 0) {
+    const {
+      name, alternate_names, species, gender, house, dateOfBirth, wizard, ancestry, hairColour,
+      hogwartsStudent, hogwartsStaff, alive, // wand, image
+    } = getChar(params.id);
 
-  return (
-    <div>
-      <h2>{name}</h2>
-      <p>{alternate_names}</p>
-      <p>{species}</p>
-      <p>{gender}</p>
-      <p>{house}</p>
-      <p>{dateOfBirth}</p>
-      <p>{wizard}</p>
-      <p>{ancestry}</p>
-      <p>{hairColour}</p>
-      <p>{hogwartsStudent}</p>
-      <p>{hogwartsStaff}</p>
-      <p>{alive}</p>
-    </div>
-  );
-};
+    return (
+      <div>
+        <h2>{name}</h2>
+        <p>{alternate_names}</p>
+        <p>{species}</p>
+        <p>{gender}</p>
+        <p>{house}</p>
+        <p>{dateOfBirth}</p>
+        <p>{wizard}</p>
+        <p>{ancestry}</p>
+        <p>{hairColour}</p>
+        <p>{hogwartsStudent}</p>
+        <p>{hogwartsStaff}</p>
+        <p>{alive}</p>
+      </div>
+    );
+  } else {
+    return null
+  }
+}
 
 export default Char;
